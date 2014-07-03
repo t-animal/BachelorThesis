04278067bd2166aa832352f03d7ae5d61bed24af
t.animal <ar79yxiw@cip.cs.fau.de>
2014-07-03 21:16:14 +0200

    Extract function into a function...

--- GoBoardReaderNative.cpp
+++ GoBoardReaderNative.cpp
@@ -26,39 +26,57 @@ int getMilliSpan(int nTimeStart) {
 	return nSpan;
 }
 
-int main(int argc, char** argv) {
-	Mat src;
-	/// Load source image and convert it to gray
-	src = imread(argv[1], 1);
+void detectVertHorzLines(Mat &img, vector<Vec4i> &horz, vector<Vec4i> &vert, float horzThreshhold = 2, float vertThreshhold = 2){
+	Mat dst;
+	vector<Vec4i> lines;
 
-	int t = getMilliCount();
+	Canny(img, dst, 50, 200, 3);
 
-	resize(src, src, Size(), 0.25, 0.25, INTER_LINEAR);
-
-	Mat dst, cdst;
-	Canny(src, dst, 50, 200, 3);
-	cvtColor(dst, cdst, CV_GRAY2BGR);
-
-	vector<Vec4i> lines, potentialLines;
 	HoughLinesP(dst, lines, 1, CV_PI / 180, 50, 50, 10);
-
 
 	for (size_t i = 0; i < lines.size(); i++) {
 		Vec4i l = lines[i];
-		Scalar other = Scalar(255, 0, 0);
-		Scalar vertical = Scalar(0, 255, 0);
-		Scalar horizontal = Scalar(0, 0, 255);
 
 		int height = l[3]-l[1];
 		int width = l[2]-l[0];
 
-		bool vert = width==0 || abs(height/width) > 2;
-		bool horz = height==0 || abs(width/height) > 2;
+		bool isVert = width==0 || abs(height/float(width)) > horzThreshhold;
+		bool isHorz = height==0 || abs(width/float(height)) > vertThreshhold;
+
+		if(isVert){
+			vert.push_back(l);
+		}else if(isHorz){
+			horz.push_back(l);
+		}
 
 		//cout << height << "||" <<width << "||" << height/width << "||" << width/height <<endl;
-
-		line(cdst, Point(l[0], l[1]), Point(l[2], l[3]), (vert?vertical:(horz?horizontal:other)), 3, CV_AA);
 	}
+}
+
+int main(int argc, char** argv) {
+	Mat src;
+	/// Load source image and convert it to gray
+	src = imread(argv[1], 1);
+	int t = getMilliCount();
+
+	resize(src, src, Size(), 0.25, 0.25, INTER_LINEAR);
+
+	vector<Vec4i> horz, vert;
+
+	detectVertHorzLines(src,horz, vert, 2.5, 2.5);
+
+	Scalar vertical = Scalar(0, 255, 0);
+	Mat cdst;
+	Canny(src, cdst, 50, 200, 3);
+	cvtColor(cdst, cdst, CV_GRAY2BGR);
+
+	for(auto h : horz){
+		line(cdst, Point(h[0], h[1]), Point(h[2], h[3]), Scalar(255, 0, 0), 3, CV_AA);
+	}
+	for(auto v : vert){
+		line(cdst, Point(v[0], v[1]), Point(v[2], v[3]), Scalar(0, 255, 000), 3, CV_AA);
+	}
+
 	cout << "Time consumed:" << getMilliSpan(t) << endl;
 
 	imshow("source", src);
