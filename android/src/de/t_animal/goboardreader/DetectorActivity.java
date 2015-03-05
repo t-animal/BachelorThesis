3d383d3c51f7875d3cbae5ac8e7e61abfecac93c
t.animal <ar79yxiw@cip.cs.fau.de>
2015-03-05 17:39:46 +0100

    use circle detection in android app

--- android/src/de/t_animal/goboardreader/DetectorActivity.java
+++ android/src/de/t_animal/goboardreader/DetectorActivity.java
@@ -9,6 +9,7 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfPoint3f;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
@@ -23,10 +24,11 @@ public class DetectorActivity extends Activity implements CvCameraViewListener2 
 	private static final String TAG = "T_ANIMAL::GBR::DetectorActivity";
 	private static final Scalar RED = new Scalar(255, 0, 0, 255);
 	private static final Scalar WHITE = new Scalar(255, 255, 255, 255);
-	private static final Scalar YELLOW = new Scalar(255, 255, 0, 255);
+	private static final Scalar DARK_GRAY = new Scalar(80, 80, 80, 255);
+	private static final Scalar LIGHT_GRAY = new Scalar(180, 180, 180, 20);
+	private static final Scalar YELLOW = new Scalar(255, 255, 0, 20);
 
-	private Mat grayImage;
-	private Mat colorImage;
+	private Mat grayImage, colorImage;
 	private CameraBridgeViewBase mOpenCvCameraView;
 
 	/**
@@ -43,6 +45,7 @@ public class DetectorActivity extends Activity implements CvCameraViewListener2 
 
 		mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.detector_activity_surface_view);
 		mOpenCvCameraView.setCvCameraViewListener(this);
+		mOpenCvCameraView.enableFpsMeter();
 	}
 
 	@Override
@@ -83,23 +86,38 @@ public class DetectorActivity extends Activity implements CvCameraViewListener2 
 
 	@Override
 	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+		colorImage = inputFrame.rgba();
 		grayImage = inputFrame.gray();
 		Imgproc.cvtColor(grayImage, colorImage, Imgproc.COLOR_GRAY2RGB);
 
 		MatOfPoint2f intersections = new MatOfPoint2f();
 		MatOfPoint2f selectedIntersections = new MatOfPoint2f();
+		MatOfPoint3f darkCircles = new MatOfPoint3f();
+		MatOfPoint3f lightCircles = new MatOfPoint3f();
 
-		detect(grayImage.getNativeObjAddr(), intersections.getNativeObjAddr(),
-				selectedIntersections.getNativeObjAddr());
+		detect(colorImage.getNativeObjAddr(), intersections.getNativeObjAddr(),
+				selectedIntersections.getNativeObjAddr(), darkCircles.getNativeObjAddr(),
+				lightCircles.getNativeObjAddr());
+		Imgproc.cvtColor(grayImage, colorImage, Imgproc.COLOR_GRAY2RGB);
 
 		for (int i = 0; i < intersections.rows(); i++) {
 			double[] p = intersections.get(i, 0);
-			Core.circle(colorImage, new Point(p[0], p[1]), 10, WHITE);
+			Core.circle(colorImage, new Point(p[0], p[1]), 10, LIGHT_GRAY, 1);
 		}
 
 		for (int i = 0; i < selectedIntersections.rows(); i++) {
 			double[] p = selectedIntersections.get(i, 0);
-			Core.circle(colorImage, new Point(p[0], p[1]), 10, YELLOW, 3);
+			Core.circle(colorImage, new Point(p[0], p[1]), 10, YELLOW, 1);
+		}
+
+		for (int i = 0; i < darkCircles.rows(); i++) {
+			double[] p = darkCircles.get(i, 0);
+			Core.circle(colorImage, new Point(p[0], p[1]), (int) p[2], DARK_GRAY, 3);
+		}
+
+		for (int i = 0; i < lightCircles.rows(); i++) {
+			double[] p = lightCircles.get(i, 0);
+			Core.circle(colorImage, new Point(p[0], p[1]), (int) p[2], WHITE, 3);
 		}
 
 		Core.circle(colorImage, new Point(colorImage.width() / 2, colorImage.height() / 2), 10, RED, 3);
@@ -128,5 +146,6 @@ public class DetectorActivity extends Activity implements CvCameraViewListener2 
 		}
 	};
 
-	public native void detect(long mgray, long intersections, long selectedIntersections);
+	public native void detect(long mgray, long intersections, long selectedIntersections, long darkCircles,
+			long lightCircles);
 }
