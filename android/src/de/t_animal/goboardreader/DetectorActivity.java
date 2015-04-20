c522b514319b35b5b5239693e6baeb548e4db939
t.animal <ar79yxiw@cip.cs.fau.de>
2015-04-20 15:02:34 +0200

    add output window to app

--- android/src/de/t_animal/goboardreader/DetectorActivity.java
+++ android/src/de/t_animal/goboardreader/DetectorActivity.java
@@ -12,8 +12,12 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfPoint3f;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 
@@ -34,7 +38,7 @@ public class DetectorActivity extends Activity implements CvCameraViewListener2 
 	private static final Scalar LIGHT_GRAY = new Scalar(180, 180, 180, 20);
 	private static final Scalar YELLOW = new Scalar(255, 255, 0, 20);
 
-	private Mat grayImage, colorImage, detectionImage;
+	private Mat grayImage, colorImage, detectionImage, output;
 	private CameraManipulatingView mOpenCvCameraView;
 	private boolean saveNextImage = false;
 
@@ -91,8 +95,8 @@ public class DetectorActivity extends Activity implements CvCameraViewListener2 
 
 	@Override
 	public void onCameraViewStarted(int width, int height) {
-		grayImage = new Mat();
-		colorImage = new Mat();
+		// grayImage = new Mat();
+		// colorImage = new Mat();
 		detectionImage = new Mat();
 
 		Camera.Parameters camParams = mOpenCvCameraView.getCameraParameters();
@@ -109,10 +113,13 @@ public class DetectorActivity extends Activity implements CvCameraViewListener2 
 		grayImage.release();
 		colorImage.release();
 		detectionImage.release();
+		output.release();
 	}
 
 	@Override
 	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+		if (output != null)
+			output.release();
 		colorImage = inputFrame.rgba();
 		grayImage = inputFrame.gray();
 		Imgproc.cvtColor(grayImage, grayImage, Imgproc.COLOR_GRAY2RGB);
@@ -149,7 +156,7 @@ public class DetectorActivity extends Activity implements CvCameraViewListener2 
 
 		int[] board = new int[81];
 		for (int i = 0; i < 81; i++) {
-			board[i] = 0;
+			board[i] = '0';
 		}
 
 		detect(detectionImage.getNativeObjAddr(), intersections.getNativeObjAddr(),
@@ -183,18 +190,76 @@ public class DetectorActivity extends Activity implements CvCameraViewListener2 
 
 		Core.circle(colorImage, new Point(colorImage.width() / 2, colorImage.height() / 2), 10, RED, 3);
 
+		Size oldSize = colorImage.size();
+
+		output = new Mat(oldSize, colorImage.type(), new Scalar(120, 120, 120));
+		Mat leftOutput = new Mat(output, new Rect(0, 0, (int) oldSize.width * 3 / 5, (int) oldSize.height));
+		Mat rightOutput = new Mat(output, new Rect((int) oldSize.width * 3 / 5, 0, (int) oldSize.width * 2 / 5,
+				(int) oldSize.height));
+
+		int colOffset = (int) (oldSize.width / 5);
+		Mat subImage = colorImage.submat(0, leftOutput.rows(), colOffset, leftOutput.cols() + colOffset);
+		subImage.copyTo(leftOutput);
+		subImage.release();
+
+		Mat boardOutput = new Mat(rightOutput, new Rect(2, (rightOutput.rows() - rightOutput.cols()) / 2 + 2,
+				rightOutput.cols() - 4, rightOutput.cols() - 4));
+		Scalar black = new Scalar(0, 0, 0);
+		Scalar white = new Scalar(255, 255, 255);
+		int randAbstand = boardOutput.cols() / 18;
+
+		for (int i = 0; i < 9; i++) {
+			Core.line(boardOutput, new Point(randAbstand + boardOutput.cols() * i / 9, randAbstand),
+					new Point(randAbstand + boardOutput.cols() * i / 9, boardOutput.rows() - randAbstand),
+					new Scalar(70, 70, 70), 2);
+			Core.line(boardOutput, new Point(randAbstand, randAbstand + boardOutput.rows() * i / 9),
+					new Point(boardOutput.cols() - randAbstand, randAbstand + boardOutput.rows() * i / 9),
+					new Scalar(70, 70, 70), 2);
+		}
+		System.out.print("board: [");
+		for (int i = 0; i < 81; i++) {
+			System.out.print(board[i] + " ");
+			if (board[i] == '0')
+				continue;
+			int row = i / 9;
+			int col = i % 9;
+			int radius = boardOutput.cols() / 18 - 10 - 2;// 10=abstand; 2=borderwidth
+			Core.circle(boardOutput,
+					new Point(boardOutput.cols() * col / 9 + randAbstand, boardOutput.rows() * row / 9 + randAbstand),
+					radius, board[i] == 'w' ? white : black, 2);
+		}
+		System.out.println("]");
+		Core.rectangle(boardOutput, new Point(0, 0), new Point(boardOutput.cols(), boardOutput.cols()),
+				new Scalar(70, 70, 70), 2);
+
 		if (saveThisImage) {
 			Mat rgbImage = new Mat();
 			Imgproc.cvtColor(colorImage, rgbImage, Imgproc.COLOR_BGR2RGB);
 
 			Highgui.imwrite(new File(storagePath, filename + "_processed.png").getPath(), rgbImage);
-			Core.subtract(Mat.ones(colorImage.size(), colorImage.type()).setTo(new Scalar(255)), colorImage, colorImage);
+			Mat subtractor = Mat.ones(colorImage.size(), colorImage.type()).setTo(new Scalar(255));
+			Core.subtract(subtractor, colorImage, colorImage);
+			subtractor.release();
 
 			rgbImage.release();
 
 		}
 
-		return colorImage;
+		intersections.release();
+		selectedIntersections.release();
+		filledIntersections.release();
+		darkCircles.release();
+		lightCircles.release();
+
+		leftOutput.release();
+		rightOutput.release();
+		boardOutput.release();
+
+		colorImage.release();
+		grayImage.release();
+		detectionImage.release();
+
+		return output;
 	}
 
 	/**
