4045b5c97aeae8161651f07616c5c939551d77e3
t.animal <ar79yxiw@cip.cs.fau.de>
2015-04-22 15:08:09 +0200

    add manual values to line evaluater

--- nativeCode/evaluation/line_evaluater.py
+++ nativeCode/evaluation/line_evaluater.py
@@ -4,16 +4,16 @@ from evaluaterUtils import *
 
 params = Storage()
 
-params.LINES_HOUGH_GAUSSKERNEL   =  range(3, 7, 2)
-params.LINES_HOUGH_GAUSSSIGMA    =  range(2, 4, 1)
-#params.LINES_HOUGH_CANNYTHRESH1  =  range(30, 70, 5)
-#params.LINES_HOUGH_CANNYTHRESH2  =  range(180, 220, 5)
-#params.LINES_HOUGH_CANNYAPERTURE =  range(2, 5, 1)
+params.LINES_HOUGH_GAUSSKERNEL   =  range(1, 8, 2)      #3 +- 4
+#params.LINES_HOUGH_GAUSSSIGMA    =  range(1, 4, 1) 		#2. +- 1
+params.LINES_HOUGH_CANNYTHRESH1  =  range(10, 91, 10)	#50 +- 40
+params.LINES_HOUGH_CANNYTHRESH2  =  range(160, 241, 10)	#200 +- 40
+#params.LINES_HOUGH_CANNYAPERTURE =  range(2, 6, 1)		#3 +- 2
 
-params.LINES_HOUGH_ANGLERES       = [180, 270, 360]
-params.LINES_HOUGH_HOUGHTHRESH    = range(25, 65, 5)
-params.LINES_HOUGH_HOUGHMINLENGTH = range(50, 90, 5)
-params.LINES_HOUGH_HOUGHMAXGAP    = range(5, 15, 3)
+params.LINES_HOUGH_ANGLERES       = [180, 360]			#180 + 180
+params.LINES_HOUGH_HOUGHTHRESH    = range(15, 61, 5)	#40 +- 25
+params.LINES_HOUGH_HOUGHMINLENGTH = range(50, 91, 5)	#70 +- 20
+params.LINES_HOUGH_HOUGHMAXGAP    = range(0, 21, 3)		#10 +- 10
 
 secret = """saimuu6ohxooRiob6ieyoocheiwiehootu6uic7nohVoh3Shie1reithu2aic8z
 			u1iJo0paik3Apu5Phadei7iewae0waeZohpaaixau7Bee5nah4ahmugaol3phoo6ong6ooyae5engo7ie
