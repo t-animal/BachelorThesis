ee6e14bd5a8397e550656b8be16f32db6a74b24d
t.animal <ar79yxiw@cip.cs.fau.de>
2015-04-23 15:19:47 +0200

    improve color detecition on borders

--- nativeCode/colorDetection.cpp
+++ nativeCode/colorDetection.cpp
@@ -1,23 +1,26 @@
 #include "colorDetection.h"
 
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
 
 #include "evaluation.h"
 
 using namespace std;
 using namespace cv;
 
-void ColorDetector::getColors(uchar *pieces){
+void ColorDetector::getColors(uchar *pieces) {
 	Mat mask;
 	src.copyTo(mask);
 
-	rectangle(mask, Point(src.cols/2-20, src.rows/2-20), Point(src.cols/2+20, src.rows/2+20), Scalar(0,0,0), -1);
-	floodFill(mask, noArray(), Point(src.cols/2, src.rows/2), Scalar(120), NULL);
+	rectangle(mask, Point(src.cols / 2 - 20, src.rows / 2 - 20), Point(src.cols / 2 + 20, src.rows / 2 + 20),
+			Scalar(0, 0, 0), -1);
+	floodFill(mask, noArray(), Point(src.cols / 2, src.rows / 2), Scalar(120), NULL);
 	bitwise_xor(src, mask, src);
 
 	uchar *start = src.datastart;
-	while(start != src.dataend){
-		if(*start == 120)
+	while (start != src.dataend) {
+		if (*start == 120)
 			*start = 0;
 		else
 			*start = 255;
@@ -26,24 +29,29 @@ void ColorDetector::getColors(uchar *pieces){
 	}
 
 	int speckleSize = Evaluater::conf("COLORS_SPECKLESIZE", 1L);
-	int rectSize = Evaluater::conf("COLORS_RECTSIZE", 31L);
+	int rectSize = Evaluater::conf("COLORS_RECTSIZE", 20L);
 	int blackThresh = Evaluater::conf("COLORS_BLACKTHRESH", 76L);
-	int whiteThresh = Evaluater::conf("COLORS_WHITETHRESH", 224L);
+	int whiteThresh = Evaluater::conf("COLORS_WHITETHRESH", 215L);
+	int borderLower = Evaluater::conf("COLORS_BORDERLOWERING", 35L);
 
 	erode(src, src, Mat(), Point(-1, -1), speckleSize);
 	dilate(src, src, Mat(), Point(-1, -1), speckleSize);
 
 	int curPiece = 0;
-	for(auto i : intersections){
+	for (auto i : intersections) {
 		Mat subPix;
 		getRectSubPix(src, Size(rectSize, rectSize), i, subPix);
-		if((sum(subPix)/400)[0] < blackThresh){
-			pieces[curPiece++] = 'b';//sum(subPix)[0]/100*(-1);
+		float pixelSum = (float)sum(subPix)[0] / rectSize/rectSize;
+		if (pixelSum < blackThresh) {
+			pieces[curPiece++] = 'b'; //sum(subPix)[0]/100*(-1);
 			continue;
 		}
 
-		if((sum(subPix)/400)[0] > whiteThresh){
-			pieces[curPiece++] = 'w';//sum(subPix)[0]/100;
+		bool isBorder = curPiece / 9 == 0 || curPiece / 9 == 8 || curPiece % 9 == 0 || curPiece % 9 == 8;
+		if(isBorder)
+			cout << pixelSum << " " << (pixelSum > whiteThresh + borderLower) << endl;
+		if (pixelSum > (whiteThresh + (isBorder?borderLower:0))) {
+			pieces[curPiece++] = 'w'; //sum(subPix)[0]/100;
 			continue;
 		}
 
