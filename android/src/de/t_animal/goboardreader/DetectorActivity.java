92baf10d44af940c3e206baaec79336f6bd64838
t.animal <ar79yxiw@cip.cs.fau.de>
2015-04-23 19:10:49 +0200

    clean up android a bit

--- android/src/de/t_animal/goboardreader/DetectorActivity.java
+++ android/src/de/t_animal/goboardreader/DetectorActivity.java
@@ -38,7 +38,7 @@ public class DetectorActivity extends Activity implements CvCameraViewListener2 
 	private static final Scalar LIGHT_GRAY = new Scalar(180, 180, 180, 20);
 	private static final Scalar YELLOW = new Scalar(255, 255, 0, 20);
 
-	private Mat grayImage, colorImage, detectionImage, output;
+	private Mat grayImage, prevGrayImage, colorImage, detectionImage, output;
 	private MatOfPoint2f prevIntersections;
 	private CameraManipulatingView mOpenCvCameraView;
 	private boolean saveNextImage = false;
@@ -112,6 +112,8 @@ public class DetectorActivity extends Activity implements CvCameraViewListener2 
 	@Override
 	public void onCameraViewStopped() {
 		grayImage.release();
+		prevGrayImage.release();
+		prevIntersections.release();
 		colorImage.release();
 		detectionImage.release();
 		output.release();
@@ -119,36 +121,7 @@ public class DetectorActivity extends Activity implements CvCameraViewListener2 
 
 	@Override
 	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-		if (output != null)
-			output.release();
-		colorImage = inputFrame.rgba();
-		grayImage = inputFrame.gray();
-		Imgproc.cvtColor(grayImage, grayImage, Imgproc.COLOR_GRAY2RGB);
-		colorImage.convertTo(detectionImage, CvType.CV_32FC4);
-
-		boolean saveThisImage = false;
-		synchronized (this) {
-			if (saveNextImage) {
-				saveThisImage = true;
-				saveNextImage = false;
-			}
-		}
-		String filename = "";
-		File storagePath = getExternalFilesDir(null);
-		if (saveThisImage) {
-			Mat rgbImage = new Mat();
-			Imgproc.cvtColor(colorImage, rgbImage, Imgproc.COLOR_BGR2RGBA);
-			filename = android.os.Build.PRODUCT.replace(" ", "-") + "__"
-					+ android.os.Build.MANUFACTURER.replace(" ", "-") + "-" + android.os.Build.MODEL.replace(" ", "-")
-					+ "__" + android.os.Build.DISPLAY.replace(" ", "-") + "_"
-					+ new SimpleDateFormat("yyyy-MM-dd--HH:mm:ss.SSS").format(new Date());
-
-			Highgui.imwrite(new File(storagePath, filename + "_unprocessed.png").getPath(), rgbImage);
-			saveAsYAML(detectionImage.getNativeObjAddr(),
-					new File(storagePath, filename + "_unprocessed.yml").getPath());
-			rgbImage.release();
-		}
-
+		// allocate and initialise the necessary memory
 		MatOfPoint2f intersections = new MatOfPoint2f();
 		MatOfPoint2f selectedIntersections = new MatOfPoint2f();
 		MatOfPoint2f filledIntersections = new MatOfPoint2f();
@@ -160,6 +133,54 @@ public class DetectorActivity extends Activity implements CvCameraViewListener2 
 			board[i] = '0';
 		}
 
+		// release the previous output in case it has not been released
+		if (output != null)
+			output.release();
+
+		// in the first frame only save the image
+		if (prevGrayImage == null) {
+			prevGrayImage = inputFrame.gray();
+			return prevGrayImage;
+		}
+
+		colorImage = inputFrame.rgba();
+		grayImage = inputFrame.gray();
+		Mat diff = new Mat(grayImage.size(), grayImage.type());
+		Core.subtract(grayImage, prevGrayImage, diff);
+		Imgproc.threshold(diff, diff, 100, 255, Imgproc.THRESH_BINARY);
+		prevGrayImage.release();
+		prevGrayImage = grayImage;
+
+		// if there's too much movement, don't detect or output anything
+		if (Core.countNonZero(diff) > 20) {
+			intersections.release();
+			selectedIntersections.release();
+			darkCircles.release();
+			lightCircles.release();
+			diff.release();
+
+			output = createOutput(colorImage, intersections, selectedIntersections, filledIntersections, darkCircles,
+					lightCircles, board);
+
+			colorImage.release();
+
+			return output;
+		}
+
+		colorImage.convertTo(detectionImage, CvType.CV_32FC4);
+
+		// check if saving was requested and perform it
+		boolean saveThisImage = false;
+		synchronized (this) {
+			if (saveNextImage) {
+				saveThisImage = true;
+				saveNextImage = false;
+			}
+		}
+		if (saveThisImage)
+			saveImage(detectionImage, true, true);
+
+		// perform the actual detection
 		if (prevIntersections == null) {
 			detect(detectionImage.getNativeObjAddr(), intersections.getNativeObjAddr(),
 					selectedIntersections.getNativeObjAddr(), filledIntersections.getNativeObjAddr(),
@@ -173,99 +194,44 @@ public class DetectorActivity extends Activity implements CvCameraViewListener2 
 		}
 		prevIntersections = filledIntersections;
 
-		for (int i = 0; i < intersections.rows(); i++) {
-			double[] p = intersections.get(i, 0);
-			Core.circle(colorImage, new Point(p[0], p[1]), 10, LIGHT_GRAY, 1);
-		}
+		// create an output image
+		output = createOutput(colorImage, intersections, selectedIntersections, filledIntersections, darkCircles,
+				lightCircles, board);
 
-		for (int i = 0; i < selectedIntersections.rows(); i++) {
-			double[] p = selectedIntersections.get(i, 0);
-			Core.circle(colorImage, new Point(p[0], p[1]), 10, YELLOW, 1);
-		}
-
-		for (int i = 0; i < darkCircles.rows(); i++) {
-			double[] p = darkCircles.get(i, 0);
-			Core.circle(colorImage, new Point(p[0], p[1]), (int) p[2], DARK_GRAY, 1);
-		}
-
-		for (int i = 0; i < lightCircles.rows(); i++) {
-			double[] p = lightCircles.get(i, 0);
-			Core.circle(colorImage, new Point(p[0], p[1]), (int) p[2], WHITE, 1);
-		}
-
-		for (int i = 0; i < filledIntersections.rows(); i++) {
-			double[] p = filledIntersections.get(i, 0);
-			Core.circle(colorImage, new Point(p[0], p[1]), 10, DARK_RED, 3);
-		}
-
-		Core.circle(colorImage, new Point(colorImage.width() / 2, colorImage.height() / 2), 10, RED, 3);
-
-		Size oldSize = colorImage.size();
-
-		output = new Mat(oldSize, colorImage.type(), new Scalar(120, 120, 120));
-		Mat leftOutput = new Mat(output, new Rect(0, 0, (int) oldSize.width * 3 / 5, (int) oldSize.height));
-		Mat rightOutput = new Mat(output, new Rect((int) oldSize.width * 3 / 5, 0, (int) oldSize.width * 2 / 5,
-				(int) oldSize.height));
-
-		int colOffset = (int) (oldSize.width / 5);
-		Mat subImage = colorImage.submat(0, leftOutput.rows(), colOffset, leftOutput.cols() + colOffset);
-		subImage.copyTo(leftOutput);
-		subImage.release();
-
-		Mat boardOutput = new Mat(rightOutput, new Rect(2, (rightOutput.rows() - rightOutput.cols()) / 2 + 2,
-				rightOutput.cols() - 4, rightOutput.cols() - 4));
-		Scalar black = new Scalar(0, 0, 0);
-		Scalar white = new Scalar(255, 255, 255);
-		int randAbstand = boardOutput.cols() / 18;
-
-		for (int i = 0; i < 9; i++) {
-			Core.line(boardOutput, new Point(randAbstand + boardOutput.cols() * i / 9, randAbstand),
-					new Point(randAbstand + boardOutput.cols() * i / 9, boardOutput.rows() - randAbstand),
-					new Scalar(70, 70, 70), 2);
-			Core.line(boardOutput, new Point(randAbstand, randAbstand + boardOutput.rows() * i / 9),
-					new Point(boardOutput.cols() - randAbstand, randAbstand + boardOutput.rows() * i / 9),
-					new Scalar(70, 70, 70), 2);
-		}
-		for (int i = 0; i < 81; i++) {
-			if (board[i] == '0')
-				continue;
-			int row = i / 9;
-			int col = i % 9;
-			int radius = boardOutput.cols() / 18 - 10 - 2;// 10=abstand; 2=borderwidth
-			Core.circle(boardOutput,
-					new Point(boardOutput.cols() * col / 9 + randAbstand, boardOutput.rows() * row / 9 + randAbstand),
-					radius, board[i] == 'w' ? white : black, 2);
-		}
-		Core.rectangle(boardOutput, new Point(0, 0), new Point(boardOutput.cols(), boardOutput.cols()),
-				new Scalar(70, 70, 70), 2);
-
+		// save the output if requested
 		if (saveThisImage) {
-			Mat rgbImage = new Mat();
-			Imgproc.cvtColor(colorImage, rgbImage, Imgproc.COLOR_BGR2RGB);
+			// String filename = android.os.Build.PRODUCT.replace(" ", "-") + "__"
+			// + android.os.Build.MANUFACTURER.replace(" ", "-") + "-"
+			// + android.os.Build.MODEL.replace(" ", "-") + "__"
+			// + android.os.Build.DISPLAY.replace(" ", "-") + "_"
+			// + new SimpleDateFormat("yyyy-MM-dd--HH:mm:ss.SSS").format(new Date());
+			// File storagePath = getExternalFilesDir(null);
+			//
+			// Mat rgbImage = new Mat();
+			// Imgproc.cvtColor(output, rgbImage, Imgproc.COLOR_BGR2RGB);
+			//
+			// Highgui.imwrite(new File(storagePath, filename + "_processed.png").getPath(), rgbImage);
+			// Mat subtractor = Mat.ones(colorImage.size(), colorImage.type()).setTo(new Scalar(255));
+			// Core.subtract(subtractor, colorImage, colorImage);
+			// subtractor.release();
+			//
+			// rgbImage.release();
 
-			Highgui.imwrite(new File(storagePath, filename + "_processed.png").getPath(), rgbImage);
-			Mat subtractor = Mat.ones(colorImage.size(), colorImage.type()).setTo(new Scalar(255));
-			Core.subtract(subtractor, colorImage, colorImage);
-			subtractor.release();
-
-			rgbImage.release();
-
+			saveImage(output, false, false);
 		}
 
+		// release the allocated memory and return
+		diff.release();
 		intersections.release();
 		selectedIntersections.release();
 		darkCircles.release();
 		lightCircles.release();
 
-		leftOutput.release();
-		rightOutput.release();
-		boardOutput.release();
-
 		colorImage.release();
-		grayImage.release();
 		detectionImage.release();
 
 		return output;
+
 	}
 
 	/**
@@ -288,6 +254,111 @@ public class DetectorActivity extends Activity implements CvCameraViewListener2 
 			}
 		}
 	};
+
+	private Mat createOutput(Mat colorImage, MatOfPoint2f intersections, MatOfPoint2f selectedIntersections,
+			MatOfPoint2f filledIntersections, MatOfPoint3f darkCircles, MatOfPoint3f lightCircles, int board[]) {
+		for (int i = 0; i < intersections.rows(); i++) {
+			double[] p = intersections.get(i, 0);
+			Core.circle(colorImage, new Point(p[0], p[1]), 10, LIGHT_GRAY, 1);
+		}
+
+		for (int i = 0; i < selectedIntersections.rows(); i++) {
+			double[] p = selectedIntersections.get(i, 0);
+			Core.circle(colorImage, new Point(p[0], p[1]), 10, YELLOW, 1);
+		}
+
+		for (int i = 0; i < darkCircles.rows(); i++) {
+			double[] p = darkCircles.get(i, 0);
+			Core.circle(colorImage,
+					new Point(p[0], p[1]), (int) p[2], DARK_GRAY, 1);
+		}
+
+		for (int i = 0; i < lightCircles.rows(); i++) {
+			double[] p = lightCircles.get(i, 0);
+			Core.circle(colorImage,
+					new Point(p[0], p[1]), (int) p[2], WHITE, 1);
+		}
+
+		for (int i = 0; i < filledIntersections.rows(); i++) {
+			double[] p = filledIntersections.get(i, 0);
+			Core.circle(colorImage, new Point(p[0], p[1]), 10, DARK_RED, 3);
+		}
+
+		Core.circle(colorImage, new Point(colorImage.width() / 2, colorImage.height() / 2), 10, RED, 3);
+
+		Size oldSize = colorImage.size();
+
+		output = new Mat(oldSize, colorImage.type(), new Scalar(120, 120, 120));
+		Mat leftOutput = new Mat(output, new
+				Rect(0, 0, (int) oldSize.width * 3 / 5, (int) oldSize.height));
+		Mat rightOutput = new Mat(output, new
+				Rect((int) oldSize.width * 3 / 5, 0, (int) oldSize.width * 2 / 5, (int) oldSize.height));
+
+		int colOffset = (int) (oldSize.width / 5);
+		Mat subImage = colorImage.submat(0, leftOutput.rows(), colOffset,
+				leftOutput.cols() + colOffset);
+		subImage.copyTo(leftOutput);
+		subImage.release();
+
+		Mat boardOutput = new Mat(rightOutput, new Rect(2, (rightOutput.rows() - rightOutput.cols()) / 2 + 2,
+				rightOutput.cols() - 4, rightOutput.cols() - 4));
+		Scalar black = new Scalar(0, 0, 0);
+		Scalar white = new
+				Scalar(255, 255, 255);
+		int randAbstand = boardOutput.cols() / 18;
+
+		for (int i = 0; i < 9; i++) {
+			Core.line(boardOutput, new Point(randAbstand + boardOutput.cols() * i / 9,
+					randAbstand),
+					new Point(randAbstand + boardOutput.cols() * i / 9, boardOutput.rows() - randAbstand), new
+					Scalar(70, 70, 70), 2);
+			Core.line(boardOutput, new Point(randAbstand, randAbstand + boardOutput.rows() * i /
+					9), new Point(boardOutput.cols() - randAbstand, randAbstand + boardOutput.rows() * i / 9),
+					new Scalar(70, 70,
+							70), 2);
+		}
+		for (int i = 0; i < 81; i++) {
+			if (board[i] == '0')
+				continue;
+			int row = i / 9;
+			int col = i % 9;
+			int radius = boardOutput.cols() / 18 - 10 - 2;// 10=abstand; 2=borderwidth
+			Core.circle(boardOutput, new
+					Point(boardOutput.cols() * col / 9 + randAbstand, boardOutput.rows() * row / 9 + randAbstand),
+					radius,
+					board[i] == 'w' ? white : black, 2);
+		}
+		Core.rectangle(boardOutput, new Point(0, 0), new
+				Point(boardOutput.cols(), boardOutput.cols()), new Scalar(70, 70, 70), 2);
+
+		leftOutput.release();
+		rightOutput.release();
+		boardOutput.release();
+
+		return output;
+	}
+
+	private void saveImage(Mat img, boolean saveYML, boolean unprocessed) {
+		String filename = android.os.Build.PRODUCT.replace(" ", "-") + "__"
+				+ android.os.Build.MANUFACTURER.replace(" ", "-") + "-"
+				+ android.os.Build.MODEL.replace(" ", "-") + "__"
+				+ android.os.Build.DISPLAY.replace(" ", "-") + "_"
+				+ new SimpleDateFormat("yyyy-MM-dd--HH:mm:ss.SSS").format(new Date());
+		File storagePath = getExternalFilesDir(null);
+
+		Mat rgbImage = new Mat();
+		Imgproc.cvtColor(img, rgbImage, Imgproc.COLOR_BGR2RGB);
+		rgbImage.convertTo(rgbImage, CvType.CV_8UC3);
+
+		Highgui.imwrite(new File(storagePath, filename + (unprocessed ? "_un" : "_") + "processed.png").getPath(),
+				rgbImage);
+
+		rgbImage.release();
+		if (saveYML)
+			saveAsYAML(img.getNativeObjAddr(), new File(storagePath, filename +
+					(unprocessed ? "_un" : "_") + "processed.yml").getPath());
+
+	}
 
 	public native void detect(long mgray, long intersections, long selectedIntersections, long filledIntersections,
 			long darkCircles, long lightCircles, int board[], long prevIntersections);
